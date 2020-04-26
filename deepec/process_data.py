8f0a79d8cf02e67ff26135114ef302e93e95f944
GIBAE KIM <anlito@kaist.ac.kr>
2020-04-26 21:18:29 +0900

    reading fasta file function for running mode

--- deepec/process_data.py
+++ deepec/process_data.py
@@ -48,6 +48,21 @@ def read_SP_Fasta(fasta_file, len_criteria=1000):
     return result
 
 
+def read_actual_Fasta(fasta_file, len_criteria=1000):
+    result = []
+    seq_ids = []
+    fp = open(fasta_file, 'r')
+    for seq_record in SeqIO.parse(fp, 'fasta'):
+        seq = seq_record.seq
+        seq_id = seq_record.id
+        if len(seq) <= len_criteria:
+            seq += '_' * (len_criteria-len(seq))
+            result.append(str(seq))
+            seq_ids.append(seq_id)
+    fp.close()
+    return result, seq_ids
+
+
 
 def readFasta(fasta_file, len_criteria=1000):
     id2seq = {}
