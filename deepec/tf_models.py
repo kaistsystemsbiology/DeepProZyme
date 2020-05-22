6d962c918f987d64ffe8b55bd0f4afa288046187
GIBAE KIM <anlito@kaist.ac.kr>
2020-05-22 13:42:34 +0900

    typo fix

--- deepec/tf_models.py
+++ deepec/tf_models.py
@@ -8,7 +8,7 @@ class DeepTFactor(nn.Module):
         super(DeepTFactor, self).__init__()
         self.explainECs = out_features
 
-        self.cnn0 = CNN0_0
+        self.cnn0 = CNN0_0()
         self.fc = nn.Linear(in_features=128*3, out_features=len(out_features))
         self.bn1 = nn.BatchNorm1d(num_features=len(out_features))
         self.out_act = nn.Sigmoid()
@@ -388,4 +388,108 @@ class InceptionEC(nn.Module):
         x = self.max_pool(x)
         x = x.view(-1, 512)
         x = self.out_act(self.bn1(self.fc(x)))
+        return x
+
+
+class DeepTFactor_gradCAM(nn.Module):
+    def __init__(self, out_features):
+        super(DeepTFactor_gradCAM, self).__init__()
+        self.explainECs = out_features
+
+        self.cnn0 = CNN0_1
+        self.fc = nn.Linear(in_features=128, out_features=len(out_features))
+        self.bn1 = nn.BatchNorm1d(num_features=len(out_features))
+        self.out_act = nn.Sigmoid()
+
+        self.cnn0.CAM_relu.register_forward_hook(self.forward_hook)
+        self.cnn0.CAM_relu.register_backward_hook(self.backward_hook)
+        self.cnn0.conv.register_forward_hook(self.forward_hook_grad)
+        self.cnn0.conv.register_backward_hook(self.backward_hook_grad)
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(m.weight)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+
+        
+    def forward(self, x):
+        x = self.cnn0(x)
+        x = x.view(-1, 128*3)
+        x = self.out_act(self.bn1(self.fc(x)))
+        return x
+
+    def forward_hook(self, _, input, output):
+        self.forward_result = torch.squeeze(output)
+
+    def backward_hook(self, _, grad_input, grad_output):
+       self.backward_result = torch.squeeze(grad_output[0])
+
+    def forward_hook_grad(self, _, input, output):
+        self.forward_result_grad = torch.squeeze(output)
+
+    def backward_hook_grad(self, _, grad_input, grad_output):
+       self.backward_result_grad = torch.squeeze(grad_output[0])
+
+
+    def get_cam(self, label_ind):
+        cam = torch.tensordot(
+            self.fc.weight[label_ind, :], 
+            self.forward_result.squeeze(), dims=1
+            ).squeeze()
+        return cam
+
+
+class CNN0_1(nn.Module):
+    '''
+    Use second level convolution.
+    channel size: 4 -> 16 
+                  8 -> 12
+                  16 -> 4
+    '''
+    def __init__(self):
+        super(CNN0_1, self).__init__()
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.1)
+           
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(4,21))
+        self.conv2 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(8,21))
+        self.conv3 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(16,21))
+
+        self.batchnorm1 = nn.BatchNorm2d(num_features=128)
+        self.batchnorm2 = nn.BatchNorm2d(num_features=128)
+        self.batchnorm3 = nn.BatchNorm2d(num_features=128)
+
+        self.conv1_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(16,1))
+        self.conv2_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(12,1))
+        self.conv3_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(4,1))
+
+        self.batchnorm1_1 = nn.BatchNorm2d(num_features=128)
+        self.batchnorm2_1 = nn.BatchNorm2d(num_features=128)
+        self.batchnorm3_1 = nn.BatchNorm2d(num_features=128)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=(1000-4+1-16+1,1), stride=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=(1000-8+1-12+1,1), stride=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=(1000-16+1-4+1,1), stride=1)
+
+        self.conv = nn.Conv2d(in_channels=128*3, out_channels=128, kernel_size=(1,1))
+        self.CAM_relu = nn.ReLU()
+        
+    def forward(self, x):
+        x1 = self.dropout(self.relu(self.batchnorm1(self.conv1(x))))
+        x2 = self.dropout(self.relu(self.batchnorm2(self.conv2(x))))
+        x3 = self.dropout(self.relu(self.batchnorm3(self.conv3(x))))
+        x1 = self.dropout(self.relu(self.batchnorm1_1(self.conv1_1(x1))))
+        x2 = self.dropout(self.relu(self.batchnorm2_1(self.conv2_1(x2))))
+        x3 = self.dropout(self.relu(self.batchnorm3_1(self.conv3_1(x3))))
+        
+        x1 = self.pool1(x1)
+        x2 = self.pool2(x2)
+        x3 = self.pool3(x3)
+
+        x = torch.cat((x1, x2, x3), dim=1)
+        x = self.CAM_relu(self.conv(x))
         return x
