dabc41eebb3076143056b4fdf86e87f591d85daa
GIBAE KIM <anlito@kaist.ac.kr>
2020-06-16 10:46:30 +0900

    Dataset class for embedding mode

--- deepec/data_loader.py
+++ deepec/data_loader.py
@@ -76,7 +76,50 @@ class ECDataset(Dataset):
 
 # create a data loading class for the use of pytorch
 # I put the one hot encoding in here
-class EnzymeDataset(Dataset):
+# class EnzymeDataset(Dataset):
+#     def __init__(self, data_X, data_Y):
+#         self.getAAmap()
+#         self.data_X = data_X
+#         self.data_Y = data_Y
+        
+    
+#     def __len__(self):
+#         return len(self.data_X)
+    
+    
+#     def getAAmap(self):
+#         aa_vocab = ['A', 'C', 'D', 'E', 
+#                     'F', 'G', 'H', 'I', 
+#                     'K', 'L', 'M', 'N', 
+#                     'P', 'Q', 'R', 'S',
+#                     'T', 'V', 'W', 'X', 
+#                     'Y', '_']
+#         map = {}
+#         for i, char in enumerate(aa_vocab):
+#             baseArray = np.zeros(len(aa_vocab)-1)
+#             if char != '_':
+#                 baseArray[i] = 1
+#             map[char] = baseArray
+#         self.map = map
+#         return
+
+        
+#     def convert2onehot(self, single_seq):
+#         single_onehot = []
+#         for x in single_seq:
+#             single_onehot.append(self.map[x])
+#         return np.asarray(single_onehot)
+    
+    
+#     def __getitem__(self, idx):
+#         x = self.data_X[idx]
+#         x = self.convert2onehot(x)
+#         y = self.data_Y[idx]
+        
+#         x = x.reshape((1,) + x.shape)
+#         y = y.reshape(-1)
+#         return x, y
+class EnzymeDataset(Dataset): # instead of one-hot encodding, test learning embedding
     def __init__(self, data_X, data_Y):
         self.getAAmap()
         self.data_X = data_X
@@ -96,9 +139,7 @@ class EnzymeDataset(Dataset):
                     'Y', '_']
         map = {}
         for i, char in enumerate(aa_vocab):
-            baseArray = np.zeros(len(aa_vocab)-1)
-            if char != '_':
-                baseArray[i] = 1
+            baseArray = i
             map[char] = baseArray
         self.map = map
         return
