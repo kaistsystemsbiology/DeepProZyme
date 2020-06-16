b0a08be2f4b475678e4cf4481b2a33fca9067fe9
GIBAE KIM <anlito@kaist.ac.kr>
2020-06-16 10:46:49 +0900

    DeepTFactor for embedding mode

--- deepec/tf_models.py
+++ deepec/tf_models.py
@@ -7,7 +7,7 @@ class DeepTFactor(nn.Module):
     def __init__(self, out_features):
         super(DeepTFactor, self).__init__()
         self.explainECs = out_features
-
+        self.embedding = nn.Embedding(1000, 21) # 20 AA + X + blank
         self.cnn0 = CNN0_1()
         self.fc = nn.Linear(in_features=128*3, out_features=len(out_features))
         self.bn1 = nn.BatchNorm1d(num_features=len(out_features))
@@ -24,59 +24,60 @@ class DeepTFactor(nn.Module):
 
         
     def forward(self, x):
+        x = self.embedding(x)
         x = self.cnn0(x)
         x = x.view(-1, 128*3)
         x = self.out_act(self.bn1(self.fc(x)))
         return x
 
-
-class CNN0_0(nn.Module):
-    '''
-    Use second level convolution.
-    channel size: 4 -> 16 
-                  8 -> 12
-                  16 -> 4
-                  1 x 1
-    '''
-    def __init__(self):
-        super(CNN0_0, self).__init__()
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.1)
+# Not used model class
+# class CNN0_0(nn.Module):
+#     '''
+#     Use second level convolution.
+#     channel size: 4 -> 16 
+#                   8 -> 12
+#                   16 -> 4
+#                   1 x 1
+#     '''
+#     def __init__(self):
+#         super(CNN0_0, self).__init__()
+#         self.relu = nn.ReLU()
+#         self.dropout = nn.Dropout(p=0.1)
            
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(4,21))
-        self.conv2 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(8,21))
-        self.conv3 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(16,21))
+#         self.conv1 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(4,21))
+#         self.conv2 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(8,21))
+#         self.conv3 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(16,21))
 
-        self.batchnorm1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm2 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm3 = nn.BatchNorm2d(num_features=128)
+#         self.batchnorm1 = nn.BatchNorm2d(num_features=128)
+#         self.batchnorm2 = nn.BatchNorm2d(num_features=128)
+#         self.batchnorm3 = nn.BatchNorm2d(num_features=128)
 
-        self.conv1_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(16,1))
-        self.conv2_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(12,1))
-        self.conv3_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(4,1))
+#         self.conv1_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(16,1))
+#         self.conv2_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(12,1))
+#         self.conv3_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(4,1))
 
-        self.batchnorm1_1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm2_1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm3_1 = nn.BatchNorm2d(num_features=128)
+#         self.batchnorm1_1 = nn.BatchNorm2d(num_features=128)
+#         self.batchnorm2_1 = nn.BatchNorm2d(num_features=128)
+#         self.batchnorm3_1 = nn.BatchNorm2d(num_features=128)
 
-        self.pool1 = nn.MaxPool2d(kernel_size=(1000-4+1-16+1,1), stride=1)
-        self.pool2 = nn.MaxPool2d(kernel_size=(1000-8+1-12+1,1), stride=1)
-        self.pool3 = nn.MaxPool2d(kernel_size=(1000-16+1-4+1,1), stride=1)
+#         self.pool1 = nn.MaxPool2d(kernel_size=(1000-4+1-16+1,1), stride=1)
+#         self.pool2 = nn.MaxPool2d(kernel_size=(1000-8+1-12+1,1), stride=1)
+#         self.pool3 = nn.MaxPool2d(kernel_size=(1000-16+1-4+1,1), stride=1)
         
-    def forward(self, x):
-        x1 = self.dropout(self.relu(self.batchnorm1(self.conv1(x))))
-        x2 = self.dropout(self.relu(self.batchnorm2(self.conv2(x))))
-        x3 = self.dropout(self.relu(self.batchnorm3(self.conv3(x))))
-        x1 = self.dropout(self.relu(self.batchnorm1_1(self.conv1_1(x1))))
-        x2 = self.dropout(self.relu(self.batchnorm2_1(self.conv2_1(x2))))
-        x3 = self.dropout(self.relu(self.batchnorm3_1(self.conv3_1(x3))))
+#     def forward(self, x):
+#         x1 = self.dropout(self.relu(self.batchnorm1(self.conv1(x))))
+#         x2 = self.dropout(self.relu(self.batchnorm2(self.conv2(x))))
+#         x3 = self.dropout(self.relu(self.batchnorm3(self.conv3(x))))
+#         x1 = self.dropout(self.relu(self.batchnorm1_1(self.conv1_1(x1))))
+#         x2 = self.dropout(self.relu(self.batchnorm2_1(self.conv2_1(x2))))
+#         x3 = self.dropout(self.relu(self.batchnorm3_1(self.conv3_1(x3))))
         
-        x1 = self.pool1(x1)
-        x2 = self.pool2(x2)
-        x3 = self.pool3(x3)
+#         x1 = self.pool1(x1)
+#         x2 = self.pool2(x2)
+#         x3 = self.pool3(x3)
 
-        x = torch.cat((x1, x2, x3), dim=1)
-        return x
+#         x = torch.cat((x1, x2, x3), dim=1)
+#         return x
 
 
 class CNN0_1(nn.Module):
@@ -164,175 +165,6 @@ class DeepEC(nn.Module):
         x = x.view(-1, 128*3)
         x = self.out_act(self.bn1(self.fc(x)))
         return x
-
-
-class CNN0_01(nn.Module):
-    '''
-    Use third level convolution.
-    channel size: 4 -> 16 
-                  8 -> 12
-                  16 -> 4
-    concat
-    channel size: 8
-    '''
-    def __init__(self):
-        super(CNN0_01, self).__init__()
-        self.relu = nn.ReLU()
-           
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(4,21))
-        self.conv2 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(8,21))
-        self.conv3 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(16,21))
-
-        self.batchnorm1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm2 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm3 = nn.BatchNorm2d(num_features=128)
-
-        self.conv1_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(16,1))
-        self.conv2_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(12,1))
-        self.conv3_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(4,1))
-
-        self.batchnorm1_1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm2_1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm3_1 = nn.BatchNorm2d(num_features=128)
-
-        self.conv4 = nn.Conv2d(in_channels=128*3, out_channels=128*3, kernel_size=(8,1))
-        self.batchnorm4 = nn.BatchNorm2d(num_features=128*3)
-
-        self.pool1 = nn.MaxPool2d(kernel_size=(1000-4+1-16+1-8+1,1), stride=1)
-        self.pool2 = nn.MaxPool2d(kernel_size=(1000-8+1-12+1-8+1,1), stride=1)
-        self.pool3 = nn.MaxPool2d(kernel_size=(1000-16+1-4+1-8+1,1), stride=1)
-        
-    def forward(self, x):
-        x1 = self.relu(self.batchnorm1(self.conv1(x)))
-        x2 = self.relu(self.batchnorm2(self.conv2(x)))
-        x3 = self.relu(self.batchnorm3(self.conv3(x)))
-        x1 = self.relu(self.batchnorm1_1(self.conv1_1(x1)))
-        x2 = self.relu(self.batchnorm2_1(self.conv2_1(x2)))
-        x3 = self.relu(self.batchnorm3_1(self.conv3_1(x3)))
-        
-        x1 = self.pool1(x1)
-        x2 = self.pool2(x2)
-        x3 = self.pool3(x3)
-
-        x = torch.cat((x1, x2, x3), dim=1)
-        x = self.relu(self.batchnorm4(self.conv4(x)))
-        return x 
-
-
-class CNN0_02(nn.Module):
-    '''
-    Use second level convolution.
-    channel size: 4 -> 16 
-                  8 -> 12
-                  16 -> 4
-    '''
-    def __init__(self):
-        super(CNN0_02, self).__init__()
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.1)
-           
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=256, kernel_size=(4,21))
-        self.conv2 = nn.Conv2d(in_channels=1, out_channels=256, kernel_size=(8,21))
-        self.conv3 = nn.Conv2d(in_channels=1, out_channels=256, kernel_size=(16,21))
-
-        self.batchnorm1 = nn.BatchNorm2d(num_features=256)
-        self.batchnorm2 = nn.BatchNorm2d(num_features=256)
-        self.batchnorm3 = nn.BatchNorm2d(num_features=256)
-
-        self.conv1_1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(16,1))
-        self.conv2_1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(12,1))
-        self.conv3_1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(4,1))
-
-        self.batchnorm1_1 = nn.BatchNorm2d(num_features=256)
-        self.batchnorm2_1 = nn.BatchNorm2d(num_features=256)
-        self.batchnorm3_1 = nn.BatchNorm2d(num_features=256)
-
-        self.pool1 = nn.MaxPool2d(kernel_size=(1000-4+1-16+1,1), stride=1)
-        self.pool2 = nn.MaxPool2d(kernel_size=(1000-8+1-12+1,1), stride=1)
-        self.pool3 = nn.MaxPool2d(kernel_size=(1000-16+1-4+1,1), stride=1)
-        
-    def forward(self, x):
-        x1 = self.dropout(self.relu(self.batchnorm1(self.conv1(x))))
-        x2 = self.dropout(self.relu(self.batchnorm2(self.conv2(x))))
-        x3 = self.dropout(self.relu(self.batchnorm3(self.conv3(x))))
-        x1 = self.dropout(self.relu(self.batchnorm1_1(self.conv1_1(x1))))
-        x2 = self.dropout(self.relu(self.batchnorm2_1(self.conv2_1(x2))))
-        x3 = self.dropout(self.relu(self.batchnorm3_1(self.conv3_1(x3))))
-        
-        x1 = self.pool1(x1)
-        x2 = self.pool2(x2)
-        x3 = self.pool3(x3)
-
-        x = torch.cat((x1, x2, x3), dim=1)
-        return x 
-
-
-class CNN0_03(nn.Module):
-    '''
-    Use third level convolution.
-    channel size: 4 -> 16 
-                  8 -> 12
-                  16 -> 4
-    concat
-    channel size: 8
-    '''
-    def __init__(self):
-        super(CNN0_03, self).__init__()
-        self.relu = nn.ReLU()
-           
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(4,21))
-        self.conv2 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(8,21))
-        self.conv3 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(16,21))
-
-        self.batchnorm1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm2 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm3 = nn.BatchNorm2d(num_features=128)
-
-        self.conv1_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(16,1))
-        self.conv2_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(12,1))
-        self.conv3_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(4,1))
-
-        self.batchnorm1_1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm2_1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm3_1 = nn.BatchNorm2d(num_features=128)
-
-        self.conv4 = nn.Conv2d(in_channels=128*3, out_channels=128*3, kernel_size=(8,1))
-        self.batchnorm4 = nn.BatchNorm2d(num_features=128*3)
-
-        self.conv5 = nn.Conv2d(in_channels=128*3, out_channels=128*3, kernel_size=(8,1))
-        self.batchnorm5 = nn.BatchNorm2d(num_features=128*3)
-
-        self.conv6 = nn.Conv2d(in_channels=128*3, out_channels=128*3, kernel_size=(8,1))
-        self.batchnorm6 = nn.BatchNorm2d(num_features=128*3)
-
-        self.conv7 = nn.Conv2d(in_channels=128*3, out_channels=128*3, kernel_size=(8,1))
-        self.batchnorm7 = nn.BatchNorm2d(num_features=128*3)
-
-        self.pool1 = nn.MaxPool2d(kernel_size=(1000-4+1-16+1-8+1,1), stride=1)
-        self.pool2 = nn.MaxPool2d(kernel_size=(1000-8+1-12+1-8+1,1), stride=1)
-        self.pool3 = nn.MaxPool2d(kernel_size=(1000-16+1-4+1-8+1,1), stride=1)
-        
-        self.pool = nn.MaxPool2d(kernel_size=(1000-16+1-4+1-8+1-8+1-8+1-8+1,1), stride=1)
-        
-    def forward(self, x):
-        x1 = self.relu(self.batchnorm1(self.conv1(x)))
-        x2 = self.relu(self.batchnorm2(self.conv2(x)))
-        x3 = self.relu(self.batchnorm3(self.conv3(x)))
-        x1 = self.relu(self.batchnorm1_1(self.conv1_1(x1)))
-        x2 = self.relu(self.batchnorm2_1(self.conv2_1(x2)))
-        x3 = self.relu(self.batchnorm3_1(self.conv3_1(x3)))
-        
-        # x1 = self.pool1(x1)
-        # x2 = self.pool2(x2)
-        # x3 = self.pool3(x3)
-
-        x = torch.cat((x1, x2, x3), dim=1)
-        x = self.relu(self.batchnorm4(self.conv4(x)))
-        x = self.relu(self.batchnorm5(self.conv5(x)))
-        x = self.relu(self.batchnorm6(self.conv6(x)))
-        x = self.relu(self.batchnorm7(self.conv7(x)))
-        x = self.pool(x)
-        return x 
 
 
 
