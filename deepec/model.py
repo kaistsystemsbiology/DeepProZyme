efe4c300f833a0b7564ed153b68a19e3811b2f08
GIBAE KIM <anlito@kaist.ac.kr>
2020-06-16 10:47:13 +0900

    delete useless models

--- deepec/model.py
+++ deepec/model.py
@@ -3,25 +3,15 @@ import torch
 import torch.nn as nn
 
 
-
-
-class DeepEC_CAM(nn.Module):
-    def __init__(self, out_features, basal_net='CNN15'):
-        super(DeepEC_CAM, self).__init__()
-        self.explainECs = None
-        self.num_ECs = out_features
-        if basal_net == 'CNN15':
-            self.cnn0 = CNN15(out_features)
-        elif basal_net == 'CNN16':
-            self.cnn0 = CNN16(out_features)
-        elif basal_net == 'ResEC':
-            self.cnn0 = ResEC(out_features , blocks=15)
-        else:
-            raise ValueError
-        self.fc = nn.Linear(in_features=out_features, out_features=out_features)
-        self.bn1 = nn.BatchNorm1d(num_features=out_features)
+class DeepECv2(nn.Module):
+    def __init__(self, out_features):
+        super(DeepECv2, self).__init__()
+        self.explainECs = out_features
+        # self.embedding = nn.Embedding(1000, 21) # 20 AA + X + blank
+        self.cnn0 = CNN0()
+        self.fc = nn.Linear(in_features=128*3, out_features=len(out_features))
+        self.bn1 = nn.BatchNorm1d(num_features=len(out_features))
         self.out_act = nn.Sigmoid()
-      
         self.init_weights()
 
 
@@ -29,66 +19,61 @@ class DeepEC_CAM(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.xavier_uniform_(m.weight)
-            if isinstance(m, nn.ConvTranspose2d):
-                nn.init.xavier_uniform_(m.weight)
             elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight)
-        
+                nn.init.xavier_uniform_(m.weight)
+
         
     def forward(self, x):
+        # x = self.embedding(x)
         x = self.cnn0(x)
-        out = torch.sum(x, axis=[2,3])
-        out = out.view(-1, self.num_ECs)
-        out = self.out_act(self.bn1(self.fc(out)))
-        return out, x
+        x = x.view(-1, 128*3)
+        x = self.out_act(self.bn1(self.fc(x)))
+        return x
 
+class CNN0(nn.Module):
+    '''
+    Use second level convolution.
+    channel size: 4 -> 16 
+                  8 -> 12
+                  16 -> 4
+    '''
+    def __init__(self):
+        super(CNN0_1, self).__init__()
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.1)
+           
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(4,21))
+        self.conv2 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(8,21))
+        self.conv3 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(16,21))
 
-class CNN16(nn.Module):
-    def __init__(self, out_feature):
-        super(CNN16, self).__init__()
-        layer1_components = [nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(4,21)),
-                             nn.BatchNorm2d(num_features=128),
-                             nn.LeakyReLU()]
+        self.batchnorm1 = nn.BatchNorm2d(num_features=128)
+        self.batchnorm2 = nn.BatchNorm2d(num_features=128)
+        self.batchnorm3 = nn.BatchNorm2d(num_features=128)
 
-        for i in range(4):
-            layer1_components.append(nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(4,1)))
-            layer1_components.append(nn.BatchNorm2d(num_features=128))
-            layer1_components.append(nn.LeakyReLU())
+        self.conv1_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(16,1))
+        self.conv2_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(12,1))
+        self.conv3_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(4,1))
 
-        self.layer1 = nn.Sequential(*layer1_components)
+        self.batchnorm1_1 = nn.BatchNorm2d(num_features=128)
+        self.batchnorm2_1 = nn.BatchNorm2d(num_features=128)
+        self.batchnorm3_1 = nn.BatchNorm2d(num_features=128)
 
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(8,21)),
-            nn.BatchNorm2d(num_features=128),
-            nn.LeakyReLU(),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(9,1)),
-            nn.BatchNorm2d(num_features=128),
-            nn.LeakyReLU()
-        )
+        self.conv = nn.Conv2d(in_channels=128*3, out_channels=128*3, kernel_size=(1,1))
+        self.batchnorm = nn.BatchNorm2d(num_features=128*3)
+        self.pool = nn.MaxPool2d(kernel_size=(982,1), stride=1)
 
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(16,21)),
-            nn.BatchNorm2d(num_features=128),
-            nn.LeakyReLU()
-        )
-
-        deconv_components = []
-        for i in range(5):
-            deconv_components += [nn.ConvTranspose2d(in_channels=128*3, out_channels=128*3, kernel_size=(4,1)),
-                                   nn.BatchNorm2d(num_features=128*3),
-                                   nn.LeakyReLU()]
-        self.deconv = nn.Sequential(*deconv_components)
-        
-        self.conv = nn.Conv2d(in_channels=128*3, out_channels=out_feature, kernel_size=(1,1))
-        
         
     def forward(self, x):
-        x1 = self.layer1(x)
-        x2 = self.layer2(x)
-        x3 = self.layer3(x)
+        x1 = self.dropout(self.relu(self.batchnorm1(self.conv1(x))))
+        x2 = self.dropout(self.relu(self.batchnorm2(self.conv2(x))))
+        x3 = self.dropout(self.relu(self.batchnorm3(self.conv3(x))))
+        x1 = self.dropout(self.relu(self.batchnorm1_1(self.conv1_1(x1))))
+        x2 = self.dropout(self.relu(self.batchnorm2_1(self.conv2_1(x2))))
+        x3 = self.dropout(self.relu(self.batchnorm3_1(self.conv3_1(x3))))
+
         x = torch.cat((x1, x2, x3), dim=1)
-        x = self.deconv(x)
-        x = self.conv(x)
+        x = self.relu(self.batchnorm(self.conv(x)))
+        x = self.pool(x)
         return x
 
 
