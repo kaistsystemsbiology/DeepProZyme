5a3d5d26a12b9c57caec267b68e932c26711f920
GIBAE KIM <anlito@kaist.ac.kr>
2020-06-22 19:45:57 +0900

    DeepECv2_2 which have two fully connected layers and have only 4*1 and 4*21 windows in conv layers

--- deepec/model.py
+++ deepec/model.py
@@ -30,6 +30,7 @@ class DeepECv2(nn.Module):
         x = self.out_act(self.bn1(self.fc(x)))
         return x
 
+
 class CNN0(nn.Module):
     '''
     Use second level convolution.
@@ -72,6 +73,77 @@ class CNN0(nn.Module):
         x3 = self.dropout(self.relu(self.batchnorm3_1(self.conv3_1(x3))))
 
         x = torch.cat((x1, x2, x3), dim=1)
+        x = self.relu(self.batchnorm(self.conv(x)))
+        x = self.pool(x)
+        return x
+
+
+class DeepECv2_2(nn.Module):
+    def __init__(self, out_features):
+        super(DeepECv2_2, self).__init__()
+        self.explainECs = out_features
+        self.cnn0 = CNN1()
+
+        self.fc1 = nn.Linear(in_features=128*2, out_features=512)
+        self.bn1 = nn.BatchNorm1d(num_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=len(out_features))
+        self.bn2 = nn.BatchNorm1d(num_features=len(out_features))
+        self.out_act = nn.Sigmoid()
+        self.relu = nn.ReLU()
+        
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(m.weight)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+
+        
+    def forward(self, x):
+        x = self.cnn0(x)
+        x = x.view(-1, 128*2)
+        x = self.relu(self.bn1(self.fc1(x)))
+        x = self.out_act(self.bn2(self.fc2(x)))
+        return x
+
+
+class CNN1(nn.Module):
+    def __init__(self):
+        super(CNN1, self).__init__()
+        conv_module = [
+            nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(4,21)),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+            nn.Dropout(p=0.1)
+        ]
+
+        conv_module += [
+            nn.Conv2d(in_channels=128, out_channels=128*2, kernel_size=(4,1)),
+            nn.BatchNorm2d(num_features=128*2),
+            nn.ReLU(),
+            nn.Dropout(p=0.1)
+        ]
+
+        conv_module += [
+            nn.Conv2d(in_channels=128*2, out_channels=128*2, kernel_size=(4,1)),
+            nn.BatchNorm2d(num_features=128*2),
+            nn.ReLU(),
+            nn.Dropout(p=0.1)
+        ] * 4
+
+        self.conv_layers = nn.Sequential(*conv_module)
+        
+        self.conv = nn.Conv2d(in_channels=128*2, out_channels=128*2, kernel_size=(1,1))
+        self.batchnorm = nn.BatchNorm2d(num_features=128*2)
+        self.pool = nn.MaxPool2d(kernel_size=(982,1), stride=1)
+        self.relu = nn.ReLU()
+
+        
+    def forward(self, x):
+        x = self.conv_layers(x)
         x = self.relu(self.batchnorm(self.conv(x)))
         x = self.pool(x)
         return x
