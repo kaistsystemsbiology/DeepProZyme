873b28df1fc73756d031743bdedc941cc4250bca
GIBAE KIM <anlito@kaist.ac.kr>
2020-09-29 20:47:27 +0900

    delete sparse dataset / use np.int64 for embedding dataset

--- deepec/data_loader.py
+++ deepec/data_loader.py
@@ -115,7 +115,7 @@ class ECEmbedDataset(Dataset):
 
 
     def convert2onehot_seq(self, single_seq, max_seq=1000):
-        single_onehot = np.zeros((max_seq))
+        single_onehot = np.zeros((max_seq), dtype=np.int64)
         for i, x in enumerate(single_seq):
             single_onehot[i] = np.asarray(self.map_AA[x])
         return single_onehot
@@ -188,75 +188,5 @@ class EnzymeDataset(Dataset):
         y = self.data_Y[idx]
         
         x = x.reshape((1,) + x.shape)
-        y = y.reshape(-1)
-        return x, y
-
-
-class ECDataset_sparse(Dataset):
-    def __init__(self, data_X, data_Y, explainECs, pred=False):
-        self.pred = pred
-        self.data_X = data_X
-        self.map_AA = self.getAAmap()
-
-        if not pred:
-            self.data_Y = data_Y
-            self.map_EC = self.getECmap(explainECs)
-        
-    
-    def __len__(self):
-        return len(self.data_X)
-    
-    
-    def getAAmap(self):
-        aa_vocab = ['A', 'C', 'D', 'E', 
-                    'F', 'G', 'H', 'I', 
-                    'K', 'L', 'M', 'N', 
-                    'P', 'Q', 'R', 'S',
-                    'T', 'V', 'W', 'X', 
-                    'Y']
-        map = {}
-        for i, char in enumerate(aa_vocab):
-            baseArray = np.zeros(len(aa_vocab))
-            baseArray[i] = 1
-            map[char] = baseArray
-        return map
-    
-    
-    def getECmap(self, explainECs):
-        ec_vocab = list(set(explainECs))
-        ec_vocab.sort()
-        map = {}
-        for i, ec in enumerate(ec_vocab):
-            baseArray = np.zeros(len(ec_vocab))
-            baseArray[i] = 1
-            map[ec] = baseArray
-        return map
-
-
-    def convert2onehot_seq(self, single_seq, max_seq=1000):
-        single_onehot = np.zeros((max_seq, len(self.map_AA)))
-        for i, x in enumerate(single_seq):
-            single_onehot[i] = np.asarray(self.map_AA[x])
-        return single_onehot
-    
-    
-    def convert2onehot_EC(self, EC):
-        map_EC = self.map_EC
-        single_onehot = np.zeros(len(map_EC))
-        for each_EC in EC:
-            single_onehot += map_EC[each_EC]
-        return single_onehot
-    
-    
-    def __getitem__(self, idx):
-        x = self.data_X[idx]
-        x = self.convert2onehot_seq(x)
-        x = x.reshape((1,) + x.shape)
-
-        if self.pred:
-            return x
-
-        y = self.data_Y[idx]
-        y = self.convert2onehot_EC(y)
         y = y.reshape(-1)
         return x, y
