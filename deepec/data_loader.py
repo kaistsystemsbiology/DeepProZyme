965330c7921050378ee70360463960330110ab31
GIBAE KIM <anlito@kaist.ac.kr>
2020-10-31 01:28:21 +0900

    encoding only 20AAs (for Enzyme Classification)

--- deepec/data_loader.py
+++ deepec/data_loader.py
@@ -140,9 +140,9 @@ class ECEmbedDataset(Dataset):
         return x, y
 
 
-# create a data loading class for the use of pytorch
-# I put the one hot encoding in here
-class EnzymeEmbedDataset(Dataset):
+######
+
+class EnzymeDataset(Dataset):
     def __init__(self, data_X, data_Y, pred=False):
         self.pred = pred
         self.data_X = data_X
@@ -164,38 +164,38 @@ class EnzymeEmbedDataset(Dataset):
                     'T', 'V', 'W', 'Y', ]
         map = {}
         for i, char in enumerate(aa_vocab):
-            baseArray = i # use this line for embedding instead of the above three
+            baseArray = np.zeros(len(aa_vocab))
+            baseArray[i] = 1 # use this line for embedding instead of the above three
             map[char] = baseArray
         return map
 
         
     def convert2onehot_seq(self, single_seq, max_seq=1000):
-        single_onehot = np.zeros((max_seq), dtype=np.int64)
+        single_onehot = np.zeros((max_seq, len(self.map_AA)))
         for i, x in enumerate(single_seq):
             single_onehot[i] = np.asarray(self.map_AA[x])
         return single_onehot
 
 
     def convert2int(self, label):
-        for y in label:
-            return np.asarray(y, dtype=np.float)
+        return np.asarray(label, dtype=np.float)
     
     
     def __getitem__(self, idx):
         x = self.data_X[idx]
         x = self.convert2onehot_seq(x)
-        x = x.reshape(1, -1)
+        x = x.reshape((1,) + x.shape)
 
         if self.pred:
             return x
+
         y = self.data_Y[idx]
         y = self.convert2int(y)
-        
         y = y.reshape(-1)
         return x, y
 
 
-class EnzymeDataset(Dataset):
+class EnzymeEmbedDataset(Dataset):
     def __init__(self, data_X, data_Y, pred=False):
         self.pred = pred
         self.data_X = data_X
@@ -214,7 +214,7 @@ class EnzymeDataset(Dataset):
                     'F', 'G', 'H', 'I', 
                     'K', 'L', 'M', 'N', 
                     'P', 'Q', 'R', 'S',
-                    'T', 'V', 'W', 'X', ]
+                    'T', 'V', 'W', 'Y', ]
         map = {}
         for i, char in enumerate(aa_vocab):
             baseArray = np.zeros(len(aa_vocab))
@@ -238,12 +238,12 @@ class EnzymeDataset(Dataset):
     def __getitem__(self, idx):
         x = self.data_X[idx]
         x = self.convert2onehot_seq(x)
-        x = x.reshape((1,) + x.shape)
+        x = x.reshape(1, -1)
 
         if self.pred:
             return x
+
         y = self.data_Y[idx]
         y = self.convert2int(y)
-
         y = y.reshape(-1)
         return x, y
