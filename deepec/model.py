7f9353147a6dcef8af973cf9b3af3f4703ccd129
anlito <anlito@kaist.ac.kr>
2021-01-27 22:34:33 +0900

    multiplied Embedding / init xavier uniform for more layers / transformer encoder yielding weight (but nelgected)

--- deepec/model.py
+++ deepec/model.py
@@ -1,9 +1,11 @@
 import math
+import copy
+from typing import Optional, Any
 # import torch packages
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import TransformerEncoder, TransformerEncoderLayer
+# from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
 
 class CNN0(nn.Module):
@@ -166,26 +168,6 @@ class DeepEC_emb(nn.Module):
         return x
 
 
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
-
-
-
 class DeepEC(nn.Module):
     def __init__(self, out_features):
         super(DeepEC, self).__init__()
@@ -231,8 +213,99 @@ class DeepEC(nn.Module):
         return x
 
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
 
-# SHORT
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+    
+class Embeddings(nn.Module):
+    def __init__(self, ntoken, ninp):
+        super(Embeddings, self).__init__()
+        self.emb = nn.Embedding(ntoken, ninp)
+        self.ninp = ninp 
+    
+    def forward(self, src):
+        return self.emb(src) * math.sqrt(self.ninp)
+    
+    
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation="relu"):
+        super(TransformerEncoderLayer, self).__init__()
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        # Implementation of Feedforward model
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
+
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.activation = self._get_activation_fn(activation)
+
+    def __setstate__(self, state):
+        if 'activation' not in state:
+            state['activation'] = F.relu
+        super(TransformerEncoderLayer, self).__setstate__(state)
+    
+    
+    def _get_activation_fn(self, activation):
+        if activation == "relu":
+            return F.relu
+        elif activation == "gelu":
+            return F.gelu
+
+        raise RuntimeError("activation should be relu/gelu, not {}".format(activation))
+
+        
+    def forward(self, src: torch.Tensor, src_mask: Optional[torch.Tensor] = None, src_key_padding_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
+        src2, weight = self.self_attn(src, src, src, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)
+        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
+        src = src + self.dropout2(src2)
+        src = self.norm2(src)
+        return src, weight
+    
+
+class TransformerEncoder(nn.Module):
+    __constants__ = ['norm']
+
+    def __init__(self, encoder_layer, num_layers, norm=None):
+        super(TransformerEncoder, self).__init__()
+        self.layers = self._get_clones(encoder_layer, num_layers)
+        self.num_layers = num_layers
+        self.norm = norm
+
+        
+    def _get_clones(self, module, N):
+        return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+    
+    def forward(self, src: torch.Tensor, mask: Optional[torch.Tensor] = None, src_key_padding_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
+        output = src
+        weights = []
+
+        for mod in self.layers:
+            output, weight = mod(output, src_mask=mask, src_key_padding_mask=src_key_padding_mask)
+            weights.append(weight)
+        if self.norm is not None:
+            output = self.norm(output)
+
+        return output, weights
+
 class DeepTransformer(nn.Module):
     def __init__(self, ntoken, ninp, nhead, nhid, nlayers, dropout=0.5, explainProts=[]):
         super(DeepTransformer, self).__init__()
@@ -243,7 +316,7 @@ class DeepTransformer(nn.Module):
         self.pos_encoder = PositionalEncoding(ninp, dropout)
         encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid, dropout)
         self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
-        self.encoder = nn.Embedding(ntoken, ninp)
+        self.encoder = Embeddings(ntoken, ninp)
         self.ninp = ninp
 
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=128*1, kernel_size=(4, ninp), stride=1)
@@ -260,19 +333,15 @@ class DeepTransformer(nn.Module):
 
 
     def init_weights(self):
-        initrange = 0.1
-        # self.encoder.weight.data.uniform_(-initrange, initrange)
-        nn.init.xavier_uniform_(self.encoder.weight) # DeepTransformer 11
-        for m in [self.fc2, self.conv1, self.conv2]:
-            nn.init.xavier_uniform_(m.weight) # DeepTransformer 11
-            # m.weight.data.uniform_(-initrange, initrange)
-            m.bias.data.zero_()
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
 
     def forward(self, src, src_mask):
         src = src.type(torch.long).T
         src = self.encoder(src) * math.sqrt(self.ninp)
         src = self.pos_encoder(src)
-        src = self.transformer_encoder(src, src_key_padding_mask=src_mask)
+        src, _ = self.transformer_encoder(src, src_key_padding_mask=src_mask)
         src.transpose_(0, 1)
         src = src.view(-1, 1, self.seq_len, self.ninp)
         src = self.relu(self.bn1(self.conv1(src)))
