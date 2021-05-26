66947682d32a90f5bceef84bca3c8cbc8672b103
anlito <anlito@kaist.ac.kr>
2021-05-26 21:23:05 +0900

    old cnn based models

--- deepec/old_models.py
+++ deepec/old_models.py
@@ -1219,3 +1219,39 @@ class CNN18(nn.Module):
         x = self.deconv2(x)
         x = self.conv(x)
         return x
+    
+    
+class ResEC(nn.Module):
+    def __init__(self, block, num_blocks, explainECs=[]):
+        super(ResEC, self).__init__()
+        self.explainECs = explainECs
+        num_classes = len(explainECs)
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=(3, 20),
+                                stride=1, padding=(1, 0), bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 256, num_blocks[3], stride=2)
+        self.linear = nn.Linear(256*block.expansion, num_classes)
+        self.pool = nn.MaxPool2d(kernel_size=(64,1), )
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
