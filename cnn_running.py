26110eaaab36b6836dff625211a391d5938e2daa
anlito <anlito@kaist.ac.kr>
2021-07-11 21:24:40 +0900

    clear script using torch.load directly

--- cnn_running.py
+++ cnn_running.py
@@ -3,26 +3,22 @@ import random
 import logging
 # import basic python packages
 import numpy as np
-from sklearn.model_selection import train_test_split
 
+from tqdm import tqdm
 # import torch packages
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
-from deepec.process_data import read_EC_actual_Fasta, \
-                                getExplainedEC_short, \
-                                convertECtoLevel3
+from deepec.process_data import read_EC_actual_Fasta
 from deepec.data_loader import ECDataset, ECEmbedDataset
 from deepec.utils import argument_parser
-from deepec.model import DeepEC
+from deepec.model import *
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
-stream_handler = logging.StreamHandler()
-logger.addHandler(stream_handler)
 
 if __name__ == '__main__':
     parser = argument_parser()
@@ -47,18 +43,12 @@ if __name__ == '__main__':
     np.random.seed(seed_num)
     torch.set_num_threads(num_cpu)
 
-    ckpt = torch.load(f'{checkpt_file}', map_location=device)
-    explainECs = ckpt['explainECs']
-
-
-    model = DeepEC(out_features=explainECs)
-    model = nn.DataParallel(model, device_ids=[0,1,2,3])
+    model = torch.load(checkpt_file)
     model = model.to(device)
-    model.load_state_dict(ckpt['model'])
+    explainECs = model.explainProts
+
 
     input_seqs, input_ids = read_EC_actual_Fasta(input_data_file)
-    _, input_seqs = train_test_split(input_seqs, test_size=0.1, random_state=seed_num)
-    _, input_ids = train_test_split(input_ids, test_size=0.1, random_state=seed_num)
     pseudo_labels = np.zeros((len(input_seqs)))
 
     # proteinDataset = ECEmbedDataset(input_seqs, pseudo_labels, explainECs, pred=True)
@@ -68,26 +58,30 @@ if __name__ == '__main__':
     model.eval() # training session with train dataset
     with torch.no_grad():
         y_pred = torch.zeros([len(input_seqs), len(explainECs)])
-        logging.info('Prediction starts on the dataset')
+        y_score = torch.zeros([len(input_seqs), len(explainECs)])
+        
         cnt = 0
-        for batch, data in enumerate(proteinDataloader):
-            data = data.type(torch.FloatTensor)
-            data = data.to(device)
-            output = model(data)
+        for batch, data in enumerate(tqdm(proteinDataloader)):
+            data = data.float()
+            output = model(data.to(device))
             prediction = torch.sigmoid(output) > 0.5
             prediction = prediction.float()
-            y_pred[cnt:cnt+data.shape[0]] = prediction.cpu()
-            cnt += data.shape[0]
+
+            step = data.size(0)
+            y_pred[cnt:cnt+step] = prediction.cpu()
+            y_score[cnt:cnt+step] = torch.sigmoid(output).cpu()
+            cnt += step
+            
         logging.info('Prediction Ended on test dataset')
 
     with open(f'{output_dir}/prediction_result.txt', 'w') as fp:
-        fp.write('sequence_ID\tprediction\n')
+        fp.write('sequence_ID\tprediction\tscore\n')
         for i, ith_pred in enumerate(y_pred):
-            if len(ith_pred.nonzero()) == 0:
-                fp.write(f'{input_ids[i]}\tNone\n')
+            nonzero_preds = torch.nonzero(ith_pred, as_tuple=False)
+            if len(nonzero_preds) == 0:
+                fp.write(f'{input_ids[i]}\tNone\t0.0\n')
                 continue
-            pred_ecs = [explainECs[j] for j in ith_pred.nonzero()]
-            pred_ecs = ';'.join(pred_ecs)
-            fp.write(f'{input_ids[i]}\t{pred_ecs}\n')
-            # for j in ith_pred.nonzero():
-            #     fp.write(f'{input_ids[i]}\t{explainECs[j]}\n')
+            for j in nonzero_preds:
+                pred_ec = explainECs[j]
+                pred_score = y_score[i][j].item()
+                fp.write(f'{input_ids[i]}\t{pred_ec}\t{pred_score:0.4f}\n')
