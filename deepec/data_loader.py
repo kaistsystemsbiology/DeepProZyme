a6938defc9713bdf8ab90354435fa5b283efeb39
anlito <anlito@kaist.ac.kr>
2021-07-11 21:20:59 +0900

    contact-map dataloader

--- deepec/data_loader.py
+++ deepec/data_loader.py
@@ -361,3 +361,65 @@ class DeepECDataset(Dataset):
         labels = labels.reshape(-1)
         sample['labels'] = torch.tensor(labels)
         return sample
+    
+    
+    
+class DeepContactECDataset(Dataset):
+    def __init__(self, data_X, data_ec, data_map, explainECs, tokenizer_name='Rostlab/prot_bert_bfd', max_length=1000, pred=False):
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, do_lower_case=False)
+        self.max_length = max_length
+        self.data_X = data_X
+        self.data_Y = data_ec
+        self.data_map = data_map
+        self.pred = pred
+        self.map_EC = self.getECmap(explainECs)
+        
+        
+    def __len__(self):
+        return len(self.data_X)
+    
+    
+    def getECmap(self, explainECs):
+        ec_vocab = list(set(explainECs))
+        ec_vocab.sort()
+        map = {}
+        for i, ec in enumerate(ec_vocab):
+            baseArray = np.zeros(len(ec_vocab))
+            baseArray[i] = 1
+            map[ec] = baseArray
+        return map
+    
+
+    def convert2onehot_EC(self, EC):
+        map_EC = self.map_EC
+        single_onehot = np.zeros(len(map_EC))
+        for each_EC in EC:
+            single_onehot += map_EC[each_EC]
+        return single_onehot
+    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+            
+        seq = " ".join(str(self.data_X[idx]))
+        seq = re.sub(r"[UZOB]", "X", seq)
+        
+        seq_ids = self.tokenizer(seq, truncation=True, padding='max_length', max_length=self.max_length)
+        
+        sample = {key: torch.tensor(val) for key, val in seq_ids.items()}
+
+        if self.pred:
+            return sample
+            
+        labels = self.data_Y[idx]
+        labels = self.convert2onehot_EC(labels)
+        labels = labels.reshape(-1)
+        sample['labels'] = torch.tensor(labels)
+        
+        dist_map = torch.tensor(self.data_map[idx]) < 8
+        seq_len = len(dist_map)
+        cnt_map = torch.zeros(self.max_length, self.max_length)
+        cnt_map[1:seq_len+1, 1:seq_len+1] = dist_map
+        
+        sample['maps'] = cnt_map.view(-1)
+        return sample
